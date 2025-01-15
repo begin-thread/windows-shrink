19645c2fffba6ef6cd27f2bbc19b0f879e2a62e7
beginthread <33784334+begin-thread@users.noreply.github.com>
2025-01-14 23:38:46 -0500

    Update _winget_install_runas_admin.bat
    added functions for better output during execution

--- _winget_install_runas_admin.bat
+++ _winget_install_runas_admin.bat
@@ -1,24 +1,39 @@
 @echo off
+cls
+
+rem *************************************************************************
+rem TESTS
+rem call :_WinGet install "Brave Browser"
+rem pause
+rem call :_WinGetWithID install "--id 9pfhdd62mxs1"
+rem pause
+rem call :_WinGet install "--id 9pfhdd62mxs1"
+rem pause
+rem goto :eof
+rem *************************************************************************
+
+echo.
 echo *************************************************************************
 echo Install useful software
 echo *************************************************************************
 
-winget install "Brave Browser"
-winget install "Mozilla Firefox"
+call :_WinGet install "Brave Browser"
+call :_WinGet install "Mozilla Firefox"
 
-winget install "Google.Chrome"
-winget install "Google.GoogleDrive"
-winget install "Google.ChromeRemoteDesktopHost"
+call :_WinGet install "Google.Chrome"
+call :_WinGet install "Google.GoogleDrive"
+call :_WinGet install "Google.ChromeRemoteDesktopHost"
 
-winget install "DominikReichl.KeePass"
+call :_WinGet install "DominikReichl.KeePass"
 
-winget install "Adobe.Acrobat.Reader.64-bit"
-winget install "Ookla.Speedtest.Desktop"
-winget install "7zip"
-winget install "lenovo vantage"
+call :_WinGet install "Adobe.Acrobat.Reader.64-bit"
+call :_WinGet install "Ookla.Speedtest.Desktop"
+call :_WinGet install "7zip"
+call :_WinGet install "lenovo vantage"
 
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install APPLE software (y/n)?"
 echo *************************************************************************
@@ -27,13 +42,13 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem Apple TV
-    winget install --id 9NM4T8B9JQZ1
+    call :_WinGetWithID install "--id 9NM4T8B9JQZ1"
 
     rem Apple Music
-    winget install --id 9pfhdd62mxs1
+    call :_WinGetWithID install "--id 9pfhdd62mxs1"
 
     rem iCloud
-    winget install --id 9PKTQ5699M62
+    call :_WinGetWithID install "--id 9PKTQ5699M62"
     rem OLD VERSION winget install Apple.iCloud
     rem ******************************   
 
@@ -43,6 +58,7 @@ if /i "%answer%"=="y" (
 
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install AUDIO-VIDEO software (y/n)?"
 echo *************************************************************************
@@ -51,8 +67,8 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem ******************************   
-    winget install "Spotify"
-    winget install "NCHSoftware.VideoPad"
+    rem call :_WinGet install "Spotify"
+    call :_WinGet install "NCHSoftware.VideoPad"
     rem ******************************   
 
 ) else if /i "%answer%"=="n" (
@@ -60,6 +76,7 @@ if /i "%answer%"=="y" (
 )
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install PROTON Suite (y/n)?"
 echo *************************************************************************
@@ -68,8 +85,8 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem ******************************   
-    winget install "Proton.ProtonVPN"
-    winget install "Proton.ProtonDrive"
+    call :_WinGet install "Proton.ProtonVPN"
+    call :_WinGet install "Proton.ProtonDrive"
     rem ******************************   
 
 ) else if /i "%answer%"=="n" (
@@ -78,6 +95,7 @@ if /i "%answer%"=="y" (
 
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install ESET software (y/n)?"
 echo *************************************************************************
@@ -86,7 +104,7 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem ******************************   
-    winget install "ESET.Security"
+    call :_WinGet install "ESET.Security"
     rem ******************************   
 
 ) else if /i "%answer%"=="n" (
@@ -95,6 +113,7 @@ if /i "%answer%"=="y" (
 
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install DEV software (y/n)?"
 echo *************************************************************************
@@ -103,9 +122,10 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem ******************************   
-    winget install "GitHub.GitHubDesktop"
-    winget install "Microsoft.VisualStudioCode"
-    winget install Slack --id 9WZDNCRDK3WP
+    call :_WinGet install "GitHub.GitHubDesktop"
+    call :_WinGet install "Microsoft.VisualStudioCode"
+    call :_WinGet install "Slack"
+    rem winget install Slack --id 9WZDNCRDK3WP
     rem ******************************   
 
 ) else if /i "%answer%"=="n" (
@@ -113,6 +133,7 @@ if /i "%answer%"=="y" (
 )
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install GAMING software (y/n)?"
 echo *************************************************************************
@@ -121,11 +142,10 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem ******************************   
-    winget install "ElectronicArts.EADesktop"
-    winget install "Blizzard.BattleNet"
-    winget install "Valve.Steam"
-    winget install "Nvidia.GeForceExperience"
-    winget install -e --id "Nvidia.GeForceNow"
+    call :_WinGet install "ElectronicArts.EADesktop"
+    call :_WinGet install "Blizzard.BattleNet"
+    call :_WinGet install "Valve.Steam"
+    call :_WinGet install "Nvidia.GeForceExperience"
     rem ******************************   
 
 ) else if /i "%answer%"=="n" (
@@ -133,6 +153,7 @@ if /i "%answer%"=="y" (
 )
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install OFFICE software (y/n)?"
 echo *************************************************************************
@@ -141,17 +162,17 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem ******************************   
-    rem BUGGED winget install "Citrix.Workspace"
-    winget install "Jabra.Direct"
-    winget install "Microsoft.Teams"
+    rem BUGGED call :_WinGet install "Citrix.Workspace"
+    call :_WinGet install "Jabra.Direct"
+    call :_WinGet install "Microsoft.Teams"
     rem ******************************   
 
 ) else if /i "%answer%"=="n" (
     echo You chose No.
 )
-echo on
 
 @echo off
+echo.
 echo *************************************************************************
 set /p answer="-> -> -> Install OpenOffice software (y/n)?"
 echo *************************************************************************
@@ -160,38 +181,48 @@ if /i "%answer%"=="y" (
     echo You chose Yes.
 
     rem ******************************   
-    winget install "Apache.OpenOffice"
+    call :_WinGet install "Apache.OpenOffice"
     rem ******************************   
 
 ) else if /i "%answer%"=="n" (
     echo You chose No.
 )
 
+@echo off
+echo.
 echo *************************************************************************
 echo Remove useless software
 echo *************************************************************************
-winget uninstall "Microsoft Sticky Notes"
-winget uninstall "Microsoft.OneDrive"
-winget uninstall "Skype"
-winget uninstall "Weather"
-winget uninstall "Windows Media Player"
-winget uninstall "Mail and Calendar"
-winget uninstall "Maps"
-winget uninstall "Paint 3D" --id 9NBLGGH5FV99
-winget uninstall "Movies & TV" --id 9WZDNCRFJ3P2
-winget uninstall "OneNote" --id XPFFZHVGQWWLHB
-winget uninstall "Microsoft.DevHome"
-winget uninstall "Solitaire & Casual Games"
-winget uninstall "Mixed Reality Portal"
-winget uninstall "OneNote for Windows 10"
-winget uninstall "Teams Machine-Wide Installer"
-winget uninstall "Apple.Bonjour"
-winget uninstall "Copilot"
-winget uninstall "Cortana"
-winget uninstall "Quick Assist"
+call :_WinGet uninstall "Microsoft Sticky Notes"
+call :_WinGet uninstall "Microsoft.OneDrive"
+call :_WinGet uninstall "Skype"
+call :_WinGet uninstall "Weather"
+call :_WinGet uninstall "Windows Media Player"
+call :_WinGet uninstall "Mail and Calendar"
+call :_WinGet uninstall "Maps"
+
+rem Paint 3D
+call :_WinGetWithID uninstall "--id 9NBLGGH5FV99"
+
+rem Movies & TV
+call :_WinGetWithID uninstall "--id 9WZDNCRFJ3P2"
+
+rem OneNote
+call :_WinGetWithID uninstall "--id XPFFZHVGQWWLHB"
+
+call :_WinGet uninstall "Microsoft.DevHome"
+call :_WinGet uninstall "Solitaire & Casual Games"
+call :_WinGet uninstall "Mixed Reality Portal"
+call :_WinGet uninstall "OneNote for Windows 10"
+call :_WinGet uninstall "Teams Machine-Wide Installer"
+call :_WinGet uninstall "Apple.Bonjour"
+call :_WinGet uninstall "Copilot"
+call :_WinGet uninstall "Cortana"
+call :_WinGet uninstall "Quick Assist"
 
 
 @echo off
+echo.
 echo *************************************************************************
 echo Stop useless services
 echo *************************************************************************
@@ -201,6 +232,7 @@ sc config "WSearch" start=disabled
 sc stop "WSearch"
 
 @echo off
+echo.
 echo *************************************************************************
 echo Remove useless startup apps
 echo *************************************************************************
@@ -211,12 +243,73 @@ reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Logishrd\LogiLDA.DLL
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\LogiLDA.dll"
 
 @echo off
+echo.
 echo *************************************************************************
 echo Hardening
 echo *************************************************************************
 
+echo.
 echo Microsoft Edge - Disable "Offer to save passwords"
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v PasswordManagerEnabled /t REG_DWORD /d 0 /f
+
+echo.
+echo Microsoft Edge - Set the Download Path
+@echo off
+set /p _download_path="-> -> -> Enter the Download path for this computer: "
+echo You entered: %_download_path%
+
+reg add "HKCU\SOFTWARE\Policies\Microsoft\Edge" /v DownloadDirectory /t REG_SZ /d %_download_path% /f
+
+goto :eof
+
+
+rem *************************************************************************
+rem FUNCTIONS
+rem *************************************************************************
+
+rem ---------------------------------------------------------------------------
+:_WinGet
+rem ---------------------------------------------------------------------------
+    if /i %1=="" (
+        echo call eof no param
+        goto :eof
+    )
+
+    if /i %2=="" (
+        echo call eof no param
+        goto :eof
+    )
+    echo.
+    echo winget %1 %2
+  
+    winget %1 %2
+    goto :eof
+   
+rem ---------------------------------------------------------------------------
+
+rem ---------------------------------------------------------------------------
+:_WinGetWithID
+rem ---------------------------------------------------------------------------
+    if /i %1=="" (
+        echo call eof no param
+        goto :eof
+    )
+
+    if /i %2=="" (
+        echo call eof no param
+        goto :eof
+    )
+
+    setlocal enabledelayedexpansion
+    set "param2=%~2"
+
+    echo.
+    echo winget %1 !%param2!
+  
+    winget %1 !%param2!
+    goto :eof
+   
+rem ---------------------------------------------------------------------------
 
 
 pause
